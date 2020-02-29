cf512dcccd8cb6672d3c4b2a40dd738430f30074
kakegawa <cmuwni@gmail.com>
2020-02-29 13:23:54 +0900

    groupコントローラーを作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  get 'group/index'
+  get 'group/show'
+  get 'group/new'
+  get 'group/create'
+  get 'group/edit'
+  get 'group/update'
+  get 'group/destroy'
   root 'users#show'
   resources :users
 end
