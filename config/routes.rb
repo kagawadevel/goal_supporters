63609666373f03a78b6dbda02b9ed2a1f58b0a05
kakegawa <cmuwni@gmail.com>
2020-02-29 13:29:08 +0900

    groupに関するルーティングを設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,6 @@
 Rails.application.routes.draw do
-  get 'group/index'
-  get 'group/show'
-  get 'group/new'
-  get 'group/create'
-  get 'group/edit'
-  get 'group/update'
-  get 'group/destroy'
+
   root 'users#show'
   resources :users
+  resources :groups
 end
