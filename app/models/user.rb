ec8cba647252fbbd97cfd10a8535a13a3d8c3fa2
kakegawa <cmuwni@gmail.com>
2020-02-29 12:29:20 +0900

    userモデルにバリデーション追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,6 @@
 class User < ApplicationRecord
-  belongs_to :group_id
+  validates :name, presence: true, length: { maximum: 30 }
+  validates :email, presence: true, length: { maximum: 255 }, uniqueness: true
+  validates :password, presence: true, length: { minimum: 6 }
+  has_secure_password
 end
