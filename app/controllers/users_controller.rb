e73c58170b70e40a516ed3321c35bb53ede95746
kakegawa <cmuwni@gmail.com>
2020-02-29 13:00:22 +0900

    ユーザーの詳細画面作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :edit, :destroy, :update]
+
   def index
   end
 
@@ -10,6 +12,12 @@ class UsersController < ApplicationController
   end
 
   def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to @user, notice: "ユーザーを作成しました"
+    else
+      render 'new'
+    end
   end
 
   def edit
@@ -20,4 +28,14 @@ class UsersController < ApplicationController
 
   def destroy
   end
+
+    private
+
+    def user_params
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
+
+    def set_user
+      @user = User.find(params[:id])
+    end
 end
