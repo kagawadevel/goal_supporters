a0d5cea2f5d8f3c98b5f4d01445b7498a52c4249
kakegawa <cmuwni@gmail.com>
2020-02-29 13:28:28 +0900

    groupコントローラーを編集

--- app/controllers/group_controller.rb
+++ app/controllers/group_controller.rb
@@ -1,4 +1,6 @@
-class GroupController < ApplicationController
+class GroupsController < ApplicationController
+  before_action :set_group, only: [:show, :edit, :destroy, :update]
+
   def index
   end
 
@@ -6,17 +8,41 @@ class GroupController < ApplicationController
   end
 
   def new
+    @group = Group.new
   end
 
   def create
+    @group = Group.new(group_params)
+    if @group.save
+      redirect_to @group, notice: "ユーザーを作成しました"
+    else
+      render 'new'
+    end
   end
 
   def edit
   end
 
   def update
+    if @group.update(group_params)
+      redirect_to group_path(@group), notice: "ユーザー情報を更新しました"
+    else
+      render 'edit'
+    end
   end
 
   def destroy
+    @group.destroy
+    redirect_to new_group_path
+  end
+
+  private
+
+  def group_params
+    params.require(:group).permit(:name, :introduce)
+  end
+
+  def set_group
+    @group = group.find(params[:id])
   end
 end
