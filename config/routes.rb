03e5526ce880a13991a51682ae2e538b45b3a27e
kakegawa <cmuwni@gmail.com>
2020-03-01 13:33:32 +0900

    goalsコントローラー作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
 
+  get 'goals/index'
+  get 'goals/show'
+  get 'goals/new'
+  get 'goals/create'
+  get 'goals/edit'
+  get 'goals/update'
+  get 'goals/destroy'
   root 'users#show'
   resources :users
   resources :groups
