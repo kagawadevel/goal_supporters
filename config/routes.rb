3f683dca12d0352f44d67a02427dfd1644b8a006
kakegawa <cmuwni@gmail.com>
2020-03-01 13:43:37 +0900

    目標登録、編集機能を作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,7 @@
 Rails.application.routes.draw do
 
-  get 'goals/index'
-  get 'goals/show'
-  get 'goals/new'
-  get 'goals/create'
-  get 'goals/edit'
-  get 'goals/update'
-  get 'goals/destroy'
   root 'users#show'
+  resources :goals
   resources :users
   resources :groups
 end
