963b5780290aa82176e484c034c8f4fa8598f496
kakegawa <cmuwni@gmail.com>
2020-03-01 14:16:06 +0900

    timelinesコントローラー作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
 
+  get 'timelines/index'
+  get 'timelines/show'
+  get 'timelines/new'
+  get 'timelines/create'
+  get 'timelines/edit'
+  get 'timelines/update'
+  get 'timelines/destroy'
   root 'users#show'
   resources :goals
   resources :users
