48a9d525b9b1c96387444cac8b1e92f755e4f230
kakegawa <cmuwni@gmail.com>
2020-03-01 18:36:13 +0900

    boardに関するviewファイルを編集

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -1,4 +1,7 @@
 class BoardsController < ApplicationController
+  before_action :set_board, only: [:show, :edit, :destroy, :update]
+  before_action :authenticate_board!
+
   def index
   end
 
@@ -6,17 +9,41 @@ class BoardsController < ApplicationController
   end
 
   def new
+    @board = Board.new
   end
 
   def create
+    @board = Board.new(board_params)
+    if @board.save
+      redirect_to @board, notice: "ユーザーを作成しました"
+    else
+      render 'new'
+    end
   end
 
   def edit
   end
 
   def update
+    if @board.update(board_params)
+      redirect_to board_path(@board), notice: "ユーザー情報を更新しました"
+    else
+      render 'edit'
+    end
   end
 
   def destroy
+    @board.destroy
+    redirect_to new_board_path
+  end
+
+  private
+
+  def board_params
+    params.require(:board).permit(:name, :email, :password, :password_confirmation)
+  end
+
+  def set_board
+    @board = Board.find(params[:id])
   end
 end
