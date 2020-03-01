d8ca90b274c54ab9220a8144886cc7dcc1868765
kakegawa <cmuwni@gmail.com>
2020-03-01 19:01:19 +0900

    boardsに関するCRUD機能作成

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -1,21 +1,22 @@
 class BoardsController < ApplicationController
   before_action :set_board, only: [:show, :edit, :destroy, :update]
-  before_action :authenticate_board!
+  before_action :authenticate_user!
 
   def index
+    @boards = Board.all
   end
 
   def show
   end
 
   def new
-    @board = Board.new
+    @board = Board.new(group_id: params[:group_id])
   end
 
   def create
     @board = Board.new(board_params)
     if @board.save
-      redirect_to @board, notice: "ユーザーを作成しました"
+      redirect_to group_path(@board.group_id), notice: "報告しました"
     else
       render 'new'
     end
@@ -26,7 +27,7 @@ class BoardsController < ApplicationController
 
   def update
     if @board.update(board_params)
-      redirect_to board_path(@board), notice: "ユーザー情報を更新しました"
+      redirect_to board_path(@board), notice: "削除しました"
     else
       render 'edit'
     end
@@ -34,13 +35,13 @@ class BoardsController < ApplicationController
 
   def destroy
     @board.destroy
-    redirect_to new_board_path
+    redirect_to group_path(@board.group_id), notice: "削除しました"
   end
 
   private
 
   def board_params
-    params.require(:board).permit(:name, :email, :password, :password_confirmation)
+    params.require(:board).permit(:title, :detail, :group_id)
   end
 
   def set_board
