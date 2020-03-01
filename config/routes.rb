c2a712ce55beb11bc61cb86a4ee49425f1896750
kakegawa <cmuwni@gmail.com>
2020-03-01 18:37:00 +0900

    borads関連のルーティングを設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,5 @@
 Rails.application.routes.draw do
 
-  get 'boards/index'
-  get 'boards/show'
-  get 'boards/new'
-  get 'boards/create'
-  get 'boards/edit'
-  get 'boards/update'
-  get 'boards/destroy'
   devise_for :users
   if Rails.env.development?
     mount LetterOpenerWeb::Engine, at: "/letter_opener"
@@ -17,4 +10,5 @@ Rails.application.routes.draw do
   resources :users
   resources :groups
   resources :timelines
+  resources :boards
 end
