1cefe846ef63e97fc3e4f2a7032095de13503a32
kakegawa <cmuwni@gmail.com>
2020-03-01 18:31:15 +0900

    boardsコントローラーを作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
 
+  get 'boards/index'
+  get 'boards/show'
+  get 'boards/new'
+  get 'boards/create'
+  get 'boards/edit'
+  get 'boards/update'
+  get 'boards/destroy'
   devise_for :users
   if Rails.env.development?
     mount LetterOpenerWeb::Engine, at: "/letter_opener"
