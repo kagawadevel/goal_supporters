e6f18f6877456f8fb31441b71df045647ea3f4ca
kakegawa <cmuwni@gmail.com>
2020-03-04 21:54:16 +0900

    グループ一覧画面のレイアウトを変更

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,10 +1,2 @@
 module ApplicationHelper
-
-  def already_joined?
-    if UserGroupRelation.find_by(user_id: current_user.id, group_id: group.id)
-      true
-    else
-      false
-    end
-  end
 end
