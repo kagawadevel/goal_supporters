709cbecbb7f692bd8be0b5de3856e90247d44763
kakegawa <cmuwni@gmail.com>
2020-03-07 12:41:05 +0900

    既にいいねされている場合、いいねボタンが表示されないよう変更

--- app/controllers/praises_controller.rb
+++ app/controllers/praises_controller.rb
@@ -7,8 +7,9 @@ class PraisesController < ApplicationController
     if my_praised.goal.user.id == current_user.id
       redirect_to group_path(params[:group_id]), flash: { danger: '自分の目標に対してはできません' }
     else
-      if my_praised.update_at
       my_praised.update(praised: my_praised.praised + 1)
+      my_timeline = Timeline.find_by(id: params[:timeline_id])
+      my_timeline.update(cheered: true)
       redirect_to group_path(params[:group_id]), flash:  { notice: 'いいね！しました' }
     end
   end
