cb92de612fd13c32fa38dee412f3327ce3ec3831
kakegawa <cmuwni@gmail.com>
2020-03-08 15:02:16 +0900

    ２４時間以内に同じ目標の報告をできない機能を修正

--- app/controllers/timelines_controller.rb
+++ app/controllers/timelines_controller.rb
@@ -1,30 +1,43 @@
 class TimelinesController < ApplicationController
   before_action :set_timeline, only: [:show, :edit, :destroy, :update]
   before_action :authenticate_user!
-  before_action :group_joined?, only: %i[new create edit update]
+  before_action :timeline_group_joined?, only: %i[create update]
+  before_action :timeline_group_joined2?, only: %i[edit destroy]
 
   def index
-    @timelines = Timeline.all
+    @timelines = Timeline.where('updated_at >= ?', 3.day.ago)
   end
 
   def show
   end
 
   def new
-    @timeline = Timeline.new(group_id: params[:group_id])
-    @my_goals= current_user.goals
+    group = Group.find_by(id: params[:group_id])
+      if group.users.where(id: current_user.id).present?
+        if current_user.goals.present?
+          @timeline = Timeline.new(group_id: params[:group_id])
+          @my_goals= current_user.goals
+        else
+          flash[:notice] = '目標をひとつも登録していません'
+          redirect_to group
+        end
+      else
+        flash[:notice] = 'グループに参加していなければそのアクションはできません'
+        redirect_to group, danger: 'グループに参加していなければそのアクションはできません'
+      end
   end
 
   def create
     @timeline = Timeline.new(timeline_params)
     my_goal = @timeline.goal
     now = Time.now
-    if my_goal.updated_at.tomorrow < now
+    if my_goal.updated_at.tomorrow < now || my_goal.created_at == my_goal.updated_at
       if @timeline.save
         my_goal.update(informed: my_goal.informed + 1)
         redirect_to group_path(@timeline.group_id), notice: "報告しました"
       else
-
+        flash[:notice] = '本文が空欄です'
+        redirect_to new_timeline_path(group_id: @timeline.group.id, user_id: current_user.id)
       end
     else
       redirect_to group_path(@timeline.group_id), notice: "その目標は前回の報告からまだ２４時間が経過していません"
@@ -32,11 +45,12 @@ class TimelinesController < ApplicationController
   end
 
   def edit
+    @my_goals= current_user.goals
   end
 
   def update
     if @timeline.update(timeline_params)
-      redirect_to group_path(@timeline.group_id), notice: "削除しました"
+      redirect_to group_path(@timeline.group_id), notice: "投稿を編集しました"
     else
       render 'edit'
     end
@@ -55,5 +69,23 @@ class TimelinesController < ApplicationController
 
   def set_timeline
     @timeline = Timeline.find(params[:id])
+  end
+
+  def timeline_group_joined?
+    group = Group.find_by(id: params[:timeline][:group_id])
+    if group.users.where(id: current_user.id).present?
+    else
+      flash[:notice] = 'グループに参加していなければそのアクションはできません'
+      redirect_to group, danger: 'グループに参加していなければそのアクションはできません'
+    end
+  end
+
+  def timeline_group_joined2?
+    group = Group.find_by(id: params[:group_id])
+    if group.users.where(id: current_user.id).present?
+    else
+      flash[:notice] = 'グループに参加していなければそのアクションはできません'
+      redirect_to group, danger: 'グループに参加していなければそのアクションはできません'
+    end
   end
 end
